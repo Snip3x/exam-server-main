a9aa7be31bf40fd500935160a23ff2a9a6dbc36c
Bilawal Imdad <Bilawalimdad25blast@gmail.com>
2022-06-07 11:10:50 +0500

    updated

--- controllers/submission.js
+++ controllers/submission.js
@@ -1,9 +1,18 @@
+const { json } = require("express/lib/response");
 const Submission = require("../models/submission");
 
 exports.postAddSubmission = async (req, res, next) => {
+  var temp = JSON.stringify(req.body)
+  var temp1 = temp.substring(2, temp.length-5)
+  console.log(temp1)
+  var temp2 = temp1.replace(/\\/g, "")
+  console.log(temp2)
+  temp2 = JSON.parse(temp2)
+  console.log(temp2)
   try {
-    const { exam_id, email, event, questionsList } = req.body;
+    const { exam_id, email, event, questionsList } = temp2;
     const submission = new Submission({ exam_id, email, event, questionsList });
+    // console.log(exam_id+email+event+questionsList)
     const result = await submission.save();
     console.log("Submission Created!");
     res.json({
